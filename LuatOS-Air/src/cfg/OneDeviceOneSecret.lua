7de4545275d3d214e000cf5997b1cd37f2049edb
yanjunjie <yanjunjie@airm2m.com>
2021-11-01 14:26:33 +0800

    add: 新增aliyuntest（未完成）

---
+++ LuatOS-Air/src/cfg/OneDeviceOneSecret.lua
@@ -0,0 +1,25 @@
+-- Aliyun配置文件
+-- Author:LuatTest
+-- CreateDate:20211025
+-- UpdateDate:20211025
+module(..., package.seeall)
+
+-- Jeremy
+AliyunInfo = {
+    ["PRODUCT_KEY"] = "a1K4TWp6E6z",
+    ["DEVICE_NAME"] = "866714049394322",
+    ["DEVICE_SECRET"] = "a044c473cdb320c9b698a3592af0e762"
+}
+
+-- 赵旭
+-- AliyunInfo = {
+--     ["PRODUCT_KEY"] = "a1HvZlnlln9",
+--     ["DEVICE_NAME"] = "866262047559884",
+--     ["DEVICE_SECRET"] = "63dedd45a60a18c0ebd921145ef10572"
+-- }
+
+-- AliyunInfo = {
+--     ["PRODUCT_KEY"] = "a1HvZlnlln9",
+--     ["DEVICE_NAME"] = "862167059989081",
+--     ["DEVICE_SECRET"] = "b3a852584f90ebe52532d00c7f93c856"
+-- }
