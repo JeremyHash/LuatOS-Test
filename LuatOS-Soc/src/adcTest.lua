3df1504d9a70b4c9506d050281ab04f0f687f7b2
yanjunjie <yanjunjie@airm2m.com>
2021-12-14 15:45:03 +0800

    add: soc-fdbTest

--- LuatOS-Soc/src/adcTest.lua
+++ LuatOS-Soc/src/adcTest.lua
@@ -7,11 +7,15 @@ local adcChannels = {}
 if MOD_TYPE == "air101" then adcChannels = {0, 1, 10} end
 
 function adcTest.test()
+    log.info(tag, "START")
+
     for k, id in pairs(adcChannels) do
         assert(adc.open(id) == true, tag .. ".open ERROR")
         log.info(tag .. ".read", adc.read(id))
         adc.close(id)
     end
+    log.info(tag, "DONE")
+
 end
 
 return adcTest
--- LuatOS-Soc/src/main.lua
+++ LuatOS-Soc/src/main.lua
@@ -5,8 +5,12 @@ MOD_TYPE = "air101"
 sys = require("sys")
 adcTest = require("adcTest")
 cryptoTest = require("cryptoTest")
+fdbTest = require("fdbTest")
+
+mcu.setClk(240)
 
 adcTest.test()
 cryptoTest.test()
+fdbTest.test()
 
 sys.run()
