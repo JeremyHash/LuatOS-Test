d14b14617f28e150358cea8db9e1f4636c446f3b
zhaoxu <zhaoxu@airm2m.com>
2021-12-17 17:45:50 +0800

    update:soc-mcuTest和soc-pinTest中添加Air103, soc-gpioTest中删掉多加的一个gpio

--- LuatOS-Soc/src/mcuTest.lua
+++ LuatOS-Soc/src/mcuTest.lua
@@ -2,7 +2,11 @@ mcuTest = {}
 
 local tag = "mcuTest"
 
-if MOD_TYPE == "air101" then clkList = {2, 80, 240} end
+if MOD_TYPE == "air101" then
+    clkList = {2, 80, 240}
+elseif MOD_TYPE == "air103" then
+    clkList = {2, 80, 240}
+end
 
 function mcuTest.test()
     log.info(tag, "START")
--- LuatOS-Soc/src/pinTest.lua
+++ LuatOS-Soc/src/pinTest.lua
@@ -21,6 +21,48 @@ if MOD_TYPE == "air101" then
         PB10 = 26,
         PB11 = 27
     }
+elseif MOD_TYPE == "air103" then
+    pinList = {
+        PA01 = 01,
+        PA02 = 02,
+        PA03 = 03,
+        PA04 = 04,
+        PA05 = 05,
+        PA06 = 06,
+        PA07 = 07,
+        PA08 = 08,
+        PA09 = 09,
+        PA10 = 10,
+        PA11 = 11,
+        PA12 = 12,
+        PA13 = 13,
+        PA14 = 14,
+        PA15 = 15,
+        PB00 = 16,
+        PB01 = 17,
+        PB02 = 18,
+        PB03 = 19,
+        PB04 = 20,
+        PB05 = 21,
+        PB06 = 22,
+        PB07 = 23,
+        PB08 = 24,
+        PB09 = 25,
+        PB10 = 26,
+        PB11 = 27,
+        PB12 = 28,
+        PB13 = 29,
+        PB14 = 30,
+        PB15 = 31,
+        PB16 = 32,
+        PB17 = 33,
+        PB18 = 34,
+        PB21 = 37,
+        PB22 = 38,
+        PB24 = 40,
+        PB25 = 41,
+        PB26 = 42
+    }
 end
 
 function pinTest.test()
