8e09eca39df517403678fa1ca9e911ed105c21e8
xingzhizhuo <xingzhizhuo@airm2m.com>
2022-01-17 10:52:02 +0800

    fix : 修改lvgl_金牛座的msgbox代码

--- LuatOS-Air/unitTest/lvgl-金牛座/Msgbox.lua
+++ LuatOS-Air/unitTest/lvgl-金牛座/Msgbox.lua
@@ -1,10 +1,15 @@
 function MsgboxInit()
-     Msgbox1 = lvgl.msgbox_create(lvgl.scr_act(),nil)
-     lvgl.obj_set_size(Msgbox1,200,200)
-	 lvgl.obj_align(Msgbox1,Father,lvgl.ALIGN_CENTER, 0, -200)
-     lvgl.msgbox_set_text(Msgbox1,"中午吃什么")
-	 lvgl.msgbox_start_auto_close(Msgbox1,5000)
+    Msgbox1 = lvgl.msgbox_create(lvgl.scr_act(),nil)
+    lvgl.obj_set_size(Msgbox1,200,200)
+	lvgl.obj_align(Msgbox1,Father,lvgl.ALIGN_CENTER, 0, -200)
+    lvgl.msgbox_set_text(Msgbox1,"中午吃什么")
+	lvgl.msgbox_set_anim_time(Msgbox1,5000)
+	lvgl.msgbox_start_auto_close(Msgbox1,5000)
 
+	Label1 = lvgl.label_create(lvgl.scr_act(),nil)
+    lvgl.label_set_text(Label1,"获取消息框的文本:"..lvgl.msgbox_get_text(Msgbox1).."\n获取用户最后“激活”按钮的索引:"..lvgl.msgbox_get_active_btn(Msgbox1).."\n获取用户最后“激活”按钮的文本:"..tostring(lvgl.msgbox_get_active_btn_text(Msgbox1)).."\n获取动画时长:"..lvgl.msgbox_get_anim_time(Msgbox1).."\n获取是否启用重新着色:"..tostring(lvgl.msgbox_get_recolor(Msgbox1)))
+    lvgl.obj_align(Label1,nil,lvgl.ALIGN_CENTER,0,-100)
+    lvgl.obj_add_style(Label1, lvgl.LABEL_PART_MAIN, demo_ThemeStyle_IndicAndFont)
 
 	Msgbox2 = lvgl.msgbox_create(lvgl.scr_act(),nil)
 	lvgl.obj_set_size(Msgbox2,200,200)
@@ -12,11 +17,15 @@ function MsgboxInit()
 	lvgl.msgbox_set_text(Msgbox2,"中午吃什么")
 	lvgl.msgbox_stop_auto_close(Msgbox2)
     -- btn = {"ok","not","haha",""}
-	-- lvgl.msgbox_add_btns(Msgbox1,"ok")
+	-- lvgl.msgbox_add_btns(Msgbox2,btn)
 	-- lvgl.msgbox_set_recolor(Msgbox2,true)
 	lvgl.msgbox_start_auto_close(Msgbox2,5000)
-	 lvgl.msgbox_stop_auto_close(Msgbox2)
+	lvgl.msgbox_stop_auto_close(Msgbox2)
 
+	Label2 = lvgl.label_create(lvgl.scr_act(),nil)
+	lvgl.label_set_text(Label2,"获取消息框的文本:"..lvgl.msgbox_get_text(Msgbox2).."\n获取用户最后“激活”按钮的索引:"..lvgl.msgbox_get_active_btn(Msgbox2).."\n获取用户最后“激活”按钮的文本:"..tostring(lvgl.msgbox_get_active_btn_text(Msgbox2)).."\n获取动画时长:"..lvgl.msgbox_get_anim_time(Msgbox2).."\n获取是否启用重新着色:"..tostring(lvgl.msgbox_get_recolor(Msgbox2)))
+	lvgl.obj_align(Label2,nil,lvgl.ALIGN_CENTER,0,200)
+	lvgl.obj_add_style(Label2, lvgl.LABEL_PART_MAIN, demo_ThemeStyle_IndicAndFont)
 
 
 end
