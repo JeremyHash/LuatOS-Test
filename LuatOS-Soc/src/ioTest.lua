e49017b8dc0eb496267ddad01714dfcf848a7714
yanjunjie <734592627@qq.com>
2022-07-06 14:08:04 +0800

    fix: ioTest 写文件方法合并

--- LuatOS-Soc/src/ioTest.lua
+++ LuatOS-Soc/src/ioTest.lua
@@ -9,59 +9,45 @@ local function readFile(filename)
         file:close()
         return res
     else
-        log.error(tag .. ".readFile." .. filename,
-                  "文件不存在或文件输入格式不正确")
+        log.error(tag .. ".readFile." .. filename, "文件不存在或文件输入格式不正确")
     end
 end
 
-local function writeFileA(filename, data)
-    local file = io.open(filename, "a")
+local function writeFile(filename, data, mode)
+    local file = io.open(filename, mode)
     if file then
         file:write(data)
         file:close()
         return true
     else
-        log.error(tag .. ".writeFileA." .. filename, "文件不存在")
+        log.error(tag .. ".writeFile." .. filename, "文件不存在")
         return false
     end
 end
 
-local function writeFileW(filename, data)
-    local file = io.open(filename, "w")
-    if file then
-        file:write(data)
-        file:close()
-        return true
-    else
-        log.error(tag .. ".writeFileW." .. filename, "文件不存在")
-        return false
-    end
+local function deleteFile(filename)
+    return os.remove(filename)
 end
-
-local function deleteFile(filename) return os.remove(filename) end
 
 local function fsTest(testPath)
-    if string.byte(testPath, -1) ~= 47 then testPath = testPath .. "/" end
+    if string.byte(testPath, -1) ~= 47 then
+        testPath = testPath .. "/"
+    end
     -- assert(writeFileW(testPath .. "FsWriteTest1.txt", "") == true,
     --        tag .. ".writeFileW ERROR")
     while true do
-        assert(writeFileA(testPath .. "FsWriteTest1.txt",
-                          "This is a FsWriteATest") == true,
-               tag .. ".writeFileA ERROR")
-        assert(writeFileW(testPath .. "FsWriteTest2.txt",
-                          "This is a FsWriteWTest") == true,
-               tag .. ".writeFileW ERROR")
-        assert(readFile(testPath .. "FsWriteTest2.txt") ==
-                   "This is a FsWriteWTest", tag .. ".readFile ERROR")
+        assert(writeFile(testPath .. "FsWriteTest1.txt", "This is a FsWriteATest", "a") == true,
+            tag .. ".writeFileA ERROR")
+        assert(writeFile(testPath .. "FsWriteTest2.txt", "This is a FsWriteWTest", "w") == true,
+            tag .. ".writeFileW ERROR")
+        assert(readFile(testPath .. "FsWriteTest2.txt") == "This is a FsWriteWTest", tag .. ".readFile ERROR")
         if io.fileSize(testPath .. "FsWriteTest1.txt") > 200 then
-            assert(io.fileSize(testPath .. "FsWriteTest1.txt") == 220,
-                   tag .. ".fileSize ERROR")
-            assert(deleteFile(testPath .. "FsWriteTest1.txt") == true,
-                   tag .. ".deleteFile ERROR")
+            assert(io.fileSize(testPath .. "FsWriteTest1.txt") == 220, tag .. ".fileSize ERROR")
+            assert(deleteFile(testPath .. "FsWriteTest1.txt") == true, tag .. ".deleteFile ERROR")
             break
         end
     end
-    writeFileW(testPath .. "FileSeekTest.txt", "FileSeekTest")
+    writeFile(testPath .. "FileSeekTest.txt", "FileSeekTest", "w")
     local file = io.open(testPath .. "FileSeekTest.txt", "r")
     assert(file:seek("end") == 12, tag .. ".seek ERROR")
     assert(file:seek("set") == 0, tag .. ".seek ERROR")
