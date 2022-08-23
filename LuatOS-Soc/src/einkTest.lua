a3eaf71e798e53c9e90badb2c611dfac44e9d2c0
yanjunjie <734592627@qq.com>
2022-08-23 17:49:54 +0800

    fix: einkTest中需要设置中文字体

--- LuatOS-Soc/src/einkTest.lua
+++ LuatOS-Soc/src/einkTest.lua
@@ -2,27 +2,31 @@ local einkTest = {}
 
 local tag = "einkTest"
 
+local initStatus = false
+
 function einkTest.test()
     if eink == nil then
         log.error(tag, "this fireware is not support eink")
         return
     end
     log.info(tag, "START")
-    eink.model(eink.MODEL_1in54)
-    if MOD_TYPE == "air101" then
-        assert(eink.setup(0, 0, 16, 19, 17, 20) == true, tag .. ".setup ERROR")
-    elseif MOD_TYPE == "ESP32C3" then
-        assert(eink.setup(0, 2, 11, 10, 6, 7) == true, tag .. ".setup ERROR")
+    if initStatus == false then
+        eink.model(eink.MODEL_1in54)
+        if MOD_TYPE == "AIR101" or MOD_TYPE == "AIR103" then
+            assert(eink.setup(0, 0, 16, 19, 17, 20) == true, tag .. ".setup ERROR")
+        elseif MOD_TYPE == "ESP32C3" then
+            assert(eink.setup(0, 2, 11, 10, 6, 7) == true, tag .. ".setup ERROR")
+        end
+        initStatus = true
     end
     local width, height, rotate = 200, 200, 1
     eink.setWin(width, height, rotate)
     local getWidth, getHeight, getRotate = eink.getWin()
-    assert(getWidth == width and getHeight == height and getRotate == rotate,
-           tag .. "getWin ERROR")
+    assert(getWidth == width and getHeight == height and getRotate == rotate, tag .. "getWin ERROR")
     eink.clear()
-    eink.print(0, 12, tag, 0, eink.font_opposansm12)
-    eink.print(0, 40, "墨水屏中文显示测试", 0,
-               eink.font_opposansm16_chinese)
+    eink.setFont(eink.font_opposansm10_chinese)
+    eink.print(0, 12, tag, 0)
+    eink.print(0, 40, "墨水屏中文显示测试", 0)
     eink.line(0, 0, 200, 200, 0)
     eink.rect(50, 50, 100, 100, 0, 1)
     eink.circle(120, 120, 20, 0, 1)
